71cb329183d59baeae0d84dfb6c3f45e29cdf8f3
Landon Schropp <schroppl@gmail.com>
2023-01-11 14:51:33 -0600

    Move Chessboard component

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,1 @@
-import classNames from "classnames";
-
-/**
- * @typedef ChessboardProps
- * @prop [className] A class name to append to the Chessboard's default class.
- */
-type ChessboardProps = {
-  className?: string
-}
-
-/**
- * @param {ChessboardProps} props
- */
-export function Chessboard({ className }: ChessboardProps) {
-  return <svg className={ classNames("chessboard", className) } viewBox="0 0 100 100">
-    <rect x="0" y="0" width="100" height="100" fill="blue" />
-  </svg>;
-}
+export * from "./components/chessboard";
