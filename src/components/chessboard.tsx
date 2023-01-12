7238b45a9d05958f1d240aac64a7610d74da94bd
Landon Schropp <schroppl@gmail.com>
2023-01-11 23:48:43 -0600

    Fix an issue in the Chessboard viewBox

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 
+import { SVG_BOARD_SIZE } from "../constants";
 import { Player } from "../types";
 import { Coordinates } from "./coordinates";
 import { Squares } from "./squares";
@@ -19,7 +20,10 @@ type ChessboardProps = {
  * @param {ChessboardProps} props
  */
 export function Chessboard({ className, orientation }: ChessboardProps) {
-  return <svg className={ classNames("chessboard", className) } viewBox="0 0 100 100">
+  return <svg
+    className={ classNames("chessboard", className) }
+    viewBox={ `0 0 ${ SVG_BOARD_SIZE } ${ SVG_BOARD_SIZE }` }
+  >
     <Squares />
     <Coordinates orientation={ orientation } />
   </svg>;
