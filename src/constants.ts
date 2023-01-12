4f15eba0e08fca62be5f49597f62b8c93466d176
Landon Schropp <schroppl@gmail.com>
2023-01-12 14:27:05 -0600

    Add additiona types and constants

--- src/constants.ts
+++ src/constants.ts
@@ -18,3 +18,15 @@ export const BOARD_SIZE = 8;
 // SVG Constants
 export const SVG_SQUARE_SIZE = 100;
 export const SVG_BOARD_SIZE = BOARD_SIZE * SVG_SQUARE_SIZE;
+
+// Squares
+export const SQUARES = [
+  "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
+  "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
+  "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
+  "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
+  "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
+  "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
+  "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
+  "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"
+] as const;
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,17 @@
-import { FILES, PLAYERS, RANKS, SQUARE_COLORS } from "./constants";
+import { FILES, PLAYERS, RANKS, SQUARE_COLORS, SQUARES } from "./constants";
 
 export type Player = typeof PLAYERS[number]
 export type Rank = typeof RANKS[number]
 export type File = typeof FILES[number]
 export type SquareColor = typeof SQUARE_COLORS[number]
+export type Square = typeof SQUARES[number]
+
+export type Highlight = {
+  square: Square,
+  type: string
+}
+
+export type Vector = [
+  number,
+  number
+];
