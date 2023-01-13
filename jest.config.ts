01196bfbc984d311e4dfd39e89c201ae352bd416
Landon Schropp <schroppl@gmail.com>
2023-01-12 20:16:14 -0600

    fixup! Add Jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest/presets/js-with-ts",
   setupFilesAfterEnv: [ "jest-extended/all", "./test/jest.setup.ts" ],
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|sass|scss)$": "identity-obj-proxy"
+  }
 };
 
 export default config;
