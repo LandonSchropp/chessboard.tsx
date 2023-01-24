60ab7f1dbcbac1357e8bce496d4c08d548ca1642
Landon Schropp <schroppl@gmail.com>
2023-01-24 14:09:57 -0600

    Add minBy function

--- src/utilities/array.ts
+++ src/utilities/array.ts
@@ -3,8 +3,30 @@
  * result as an array.
  * @param count The number of times the transform function could be called.
  * @param transform The function whose results determined the returned values.
- * @return Returns the array of results from calling the transform function.
+ * @returns Returns the array of results from calling the transform function.
  */
 export function times<T>(count: number, transform: (index: number) => T) {
   return Array(count).fill(null).map((value, index) => transform(index));
+}
+
+/**
+ * Returns the value with the minimum result.
+ * @param array The array to search.
+ * @param minimum The function whose results determines how the value of the object is defined.
+ * @returns The minimum value in the array, or undefined if there is no minimum value.
+ */
+export function minBy<T>(array: T[], transform: (value: T) => number): T | undefined {
+  let accumulator: T | undefined;
+  let accumulatedValue = Number.POSITIVE_INFINITY;
+
+  for (const value of array) {
+    const transformedValue = transform(value);
+
+    if (transformedValue < accumulatedValue) {
+      accumulator = value;
+      accumulatedValue = transformedValue;
+    }
+  }
+
+  return accumulator;
 }
--- test/utilities/array.test.ts
+++ test/utilities/array.test.ts
@@ -1,4 +1,4 @@
-import { times } from "../../src/utilities/array";
+import { minBy, times } from "../../src/utilities/array";
 
 function double(value: number) {
   return value * 2;
@@ -23,6 +23,29 @@ describe("times", () => {
 
     it("returns an array whose values are the indices transformed by the provided function", () => {
       expect(times(3, double)).toEqual([ 0, 2, 4 ]);
+    });
+  });
+});
+
+describe("minBy", () => {
+  describe("when the array is empty", () => {
+
+    it("returns undefined", () => {
+      expect(minBy([], value => value)).toBeUndefined();
+    });
+  });
+
+  describe("when the transform function only returns Number.POSITIVE_INFINITY", () => {
+
+    it("returns undefined", () => {
+      expect(minBy([ 1, 2, 3 ], () => Number.POSITIVE_INFINITY)).toBeUndefined();
+    });
+  });
+
+  describe("when the transform function returns values less than Number.POSITIVE_INFINITY", () => {
+
+    it("returns the minimum value", () => {
+      expect(minBy([ 1, 2, 3 ], value => -value)).toEqual(3);
     });
   });
 });
