b61d394775af2616da36e387398452eca39c9343
Landon Schropp <schroppl@gmail.com>
2023-06-29 00:59:09 -0700

    Migrate to Storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,20 @@
 const path = require("path");
-
 module.exports = {
   core: {
     builder: "webpack5"
   },
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     config.module.rules.push({
       test: /\.s[ac]ss$/,
-      use: [ "style-loader", "css-loader", "sass-loader" ],
+      use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../")
     });
     return config;
+  },
+  docs: {
+    autodocs: true
   }
 };
