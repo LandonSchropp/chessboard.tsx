6359ddff4011ebffc5a4c930f6badee8f4baf8a9
Landon Schropp <schroppl@gmail.com>
2023-07-08 00:06:06 -0700

    Drop a story for Chessboard

--- src/stories/chessboard.stories.tsx
+++ src/stories/chessboard.stories.tsx
@@ -19,16 +19,3 @@ export function StaticChessboard() {
     orientation={ WHITE }
   />;
 }
-
-export function MovableChessboard() {
-  return <Chessboard
-    fen={ STARTING_POSITION }
-    orientation={ WHITE }
-    interactive
-    onHighlight={ ({ square }) => console.log(`Highlighting ${ square }`) }
-    onArrow={ ({ from, to }) => console.log(`Arrow from ${ from } to ${ to }`) }
-    onSelect={ ({ square }) => console.log(`Selecting ${ square }`) }
-    onDeselect={ () => console.log(`Deselecting`) }
-    onMove={ ({ from, to }) => { console.log(`Moving from ${ from } to ${ to }`); return false; } }
-  />;
-}
