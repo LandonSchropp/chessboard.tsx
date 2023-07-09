2a344fa00f36f7d55e9c2e6606c04a232c8f28a3
Landon Schropp <schroppl@gmail.com>
2023-07-09 00:31:20 -0700

    Add first interactive chessboard story

--- src/stories/chessboard.stories.tsx
+++ src/stories/chessboard.stories.tsx
@@ -1,5 +1,11 @@
+import { Chess } from "chess.js";
+import { useReducer } from "react";
+
 import { Chessboard } from "../components/chessboard";
 import { EMPTY_POSITION, STARTING_POSITION, WHITE } from "../constants";
+import { useArrowReducer } from "../hooks/use-arrow-reducer";
+import { useHighlightReducer } from "../hooks/use-highlight-reducer";
+import { MoveEvent } from "../types";
 
 export default {
   title: "Chessboard",
@@ -16,6 +22,41 @@ export function EmptyChessboard() {
 export function StaticChessboard() {
   return <Chessboard
     fen={ STARTING_POSITION }
+    orientation={ WHITE }
+  />;
+}
+
+/**
+ * A reducer that takes a FEN string and a move event and returns a new FEN string with the move
+ * applied. This reducer does not check for move legality.
+ */
+function moveReducer(fen: string, move: MoveEvent): string {
+  const chess = new Chess(fen);
+
+  const piece = chess.remove(move.from);
+  chess.put(piece, move.to);
+
+  return chess.fen();
+}
+
+export function InteractiveChessboard() {
+  const [ highlights, handleHighlight ] = useHighlightReducer();
+  const [ arrows, handleArrow ] = useArrowReducer();
+  const [ fen, dispatchMove ] = useReducer(moveReducer, STARTING_POSITION);
+
+  function handleMove(move: MoveEvent) {
+    dispatchMove(move);
+    return true;
+  }
+
+  return <Chessboard
+    fen={ fen }
+    interactive
+    highlights={ highlights }
+    arrows={ arrows }
+    onHighlight={ handleHighlight }
+    onArrow={ handleArrow }
+    onMove={ handleMove }
     orientation={ WHITE }
   />;
 }
