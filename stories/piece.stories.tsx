3567b343f03d4aa15f339ee2f920a583c05729db
Landon Schropp <schroppl@gmail.com>
2023-07-15 00:23:16 -0700

    Update Piece stories

--- stories/piece.stories.tsx
+++ stories/piece.stories.tsx
@@ -1,35 +1,100 @@
+import { StoryObj } from "@storybook/react";
+import { merge } from "lodash";
+
 import { Piece } from "../src/components/piece";
-import { BLACK, PIECES, SQUARES, WHITE } from "../src/constants";
-import { Player } from "../src/types";
+import {
+  BLACK_BISHOP,
+  BLACK_KING,
+  BLACK_KNIGHT,
+  BLACK_PAWN,
+  BLACK_QUEEN,
+  BLACK_ROOK,
+  WHITE,
+  WHITE_BISHOP,
+  WHITE_KING,
+  WHITE_KNIGHT,
+  WHITE_PAWN,
+  WHITE_QUEEN,
+  WHITE_ROOK
+} from "../src/constants";
+import { SVGSquareDecorator } from "./decorators/svg-decorators";
 
 export default {
-  title: "Piece",
-  component: Piece
+  title: "Internal/Piece",
+  component: Piece,
+  decorators: [ SVGSquareDecorator ]
 };
 
-function Pieces({ orientation }: { orientation: Player }) {
-  return <>
-    {
-      SQUARES.map((square, index) => {
-        return <Piece
-          key={ square }
-          square={ square }
-          piece={ PIECES[index % PIECES.length]! }
-          orientation={ orientation }
-        />;
-      })
-    }
-  </>;
-}
+export const WhiteKing: StoryObj<typeof Piece> = {
+  args: {
+    piece: WHITE_KING,
+    square: "a1",
+    orientation: WHITE
+  }
+};
 
-export function WhitePieces() {
-  return <Pieces orientation={ WHITE } />;
-}
+export const WhiteQueen: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: WHITE_QUEEN
+  }
+});
 
-WhitePieces.storyName = "Pieces (White)";
+export const WhiteRook: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: WHITE_ROOK
+  }
+});
 
-export function BlackPieces() {
-  return <Pieces orientation={ BLACK } />;
-}
+export const WhiteBishop: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: WHITE_BISHOP
+  }
+});
 
-BlackPieces.storyName = "Pieces (Black)";
+export const WhiteKnight: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: WHITE_KNIGHT
+  }
+});
+
+export const WhitePawn: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: WHITE_PAWN
+  }
+});
+
+export const BlackKing: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: BLACK_KING
+  }
+});
+
+export const BlackQueen: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: BLACK_QUEEN
+  }
+});
+
+export const BlackRook: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: BLACK_ROOK
+  }
+});
+
+export const BlackBishop: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: BLACK_BISHOP
+  }
+});
+
+export const BlackKnight: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: BLACK_KNIGHT
+  }
+});
+
+export const BlackPawn: StoryObj<typeof Piece> = merge({}, WhiteKing, {
+  args: {
+    piece: BLACK_PAWN
+  }
+});
--- stories/pieces.stories.tsx
+++ stories/pieces.stories.tsx
@@ -1,37 +1,46 @@
-import { useEffect, useState } from "react";
+import { StoryObj } from "@storybook/react";
+import { merge } from "lodash";
+import React, { useEffect, useState } from "react";
 
 import { Pieces } from "../src/components/pieces";
 import { BLACK, EMPTY_POSITION, PIECES, STARTING_POSITION, WHITE } from "../src/constants";
+import { SVGBoardDecorator } from "./decorators/svg-decorators";
 
 export default {
-  title: "Pieces",
-  component: Pieces
+  title: "Internal/Pieces",
+  component: Pieces,
+  decorators: [ SVGBoardDecorator ]
 };
 
-export function EmptyPieces() {
-  return <Pieces
-    fen={ EMPTY_POSITION }
-    orientation={ WHITE }
-  />;
-}
+/**
+ * No pieces.
+ */
+export const Empty: StoryObj<typeof Pieces> = {
+  args: {
+    fen: EMPTY_POSITION,
+    orientation: WHITE
+  }
+};
 
-export function WhiteStartingPosition() {
-  return <Pieces
-    fen={ STARTING_POSITION }
-    orientation={ WHITE }
-  />;
-}
+/**
+ * The starting position from White's perspective.
+ */
+export const WhiteStartingPosition: StoryObj<typeof Pieces> = merge({}, Empty, {
+  name: "Starting Position (White)",
+  args: {
+    fen: STARTING_POSITION
+  }
+});
 
-WhiteStartingPosition.storyName = "Starting Position (White)";
-
-export function BlackStartingPosition() {
-  return <Pieces
-    fen={ STARTING_POSITION }
-    orientation={ BLACK }
-  />;
-}
-
-BlackStartingPosition.storyName = "Starting Position (Black)";
+/**
+ * The starting position from Black's perspective.
+ */
+export const BlackStartingPosition: StoryObj<typeof Pieces> = merge({}, WhiteStartingPosition, {
+  name: "Starting Position (Black)",
+  args: {
+    orientation: BLACK
+  }
+});
 
 function AnimatedPieces({ fens }: { fens: string[] }) {
   const [ index, setIndex ] = useState(0);
@@ -50,87 +59,103 @@ function AnimatedPieces({ fens }: { fens: string[] }) {
   />;
 }
 
-export function ItalianAnimation() {
-  const fens = [
-    STARTING_POSITION,
-    "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
-    "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2",
-    "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
-    "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3",
-    "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",
-    "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",
-    "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3"
-  ];
+/**
+ * The Italian opening animated.
+ */
+export const ItalianAnimation: StoryObj<typeof Pieces> = merge({}, Empty, {
+  name: "Italian Opening (Animated)",
+  render: () => {
+    const fens = [
+      STARTING_POSITION,
+      "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+      "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2",
+      "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
+      "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3",
+      "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",
+      "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3",
+      "r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3"
+    ];
 
-  return <AnimatedPieces fens={ fens } />;
-}
+    return <AnimatedPieces fens={ fens } />;
+  }
+});
 
-ItalianAnimation.storyName = "Italian Opening (Animated)";
+/**
+ * Castling animations.
+ */
+export const CastlingAnimation: StoryObj<typeof Pieces> = merge({}, Empty, {
+  name: "Castling (Animated)",
+  render: () => {
+    const fens = [
+      "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 0 1",
+      "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R4RK1 b kq - 1 1",
+      "r4rk1/pppppppp/8/8/8/8/PPPPPPPP/R4RK1 w - - 2 2",
+      "r4rk1/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQ - 3 2",
+      "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 4 3",
+      "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/2KR3R b kq - 5 3",
+      "2kr3r/pppppppp/8/8/8/8/PPPPPPPP/2KR3R w KQ - 6 4",
+      "2kr3r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w - - 7 4"
+    ];
 
-export function CastlingAnimation() {
-  const fens = [
-    "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 0 1",
-    "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R4RK1 b kq - 1 1",
-    "r4rk1/pppppppp/8/8/8/8/PPPPPPPP/R4RK1 w - - 2 2",
-    "r4rk1/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQ - 3 2",
-    "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R b KQkq - 4 3",
-    "r3k2r/pppppppp/8/8/8/8/PPPPPPPP/2KR3R b kq - 5 3",
-    "2kr3r/pppppppp/8/8/8/8/PPPPPPPP/2KR3R w KQ - 6 4",
-    "2kr3r/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w - - 7 4"
-  ];
+    return <AnimatedPieces fens={ fens } />;
+  }
+});
 
-  return <AnimatedPieces fens={ fens } />;
-}
+/**
+ * Appearing and disappearing animations.
+ */
+export const AppearingAndDisappearingAnimation: StoryObj<typeof Pieces> = merge({}, Empty, {
+  name: "Appearing and Disappearing (Animated)",
+  render: () => {
+    const fens = [
+      EMPTY_POSITION,
+      "n7/n7/n7/n7/n7/n7/n7/n7 w - - 0 1",
+      "nN6/nN6/nN6/nN6/nN6/nN6/nN6/nN6 w - - 0 1",
+      "nNb5/nNb5/nNb5/nNb5/nNb5/nNb5/nNb5/nNb5 w - - 0 1",
+      "nNbB4/nNbB4/nNbB4/nNbB4/nNbB4/nNbB4/nNbB4/nNbB4 w - - 0 1",
+      "nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3 w - - 0 1",
+      "nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2 w - - 0 1",
+      "nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1 w - - 0 1",
+      "nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ w - - 0 1",
+      "1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ w - - 0 1",
+      "2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ w - - 0 1",
+      "3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ w - - 0 1",
+      "4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ w - - 0 1",
+      "5RqQ/5RqQ/5RqQ/5RqQ/5RqQ/5RqQ/5RqQ/5RqQ w - - 0 1",
+      "6qQ/6qQ/6qQ/6qQ/6qQ/6qQ/6qQ/6qQ w - - 0 1",
+      "7Q/7Q/7Q/7Q/7Q/7Q/7Q/7Q w - - 0 1"
+    ];
 
-CastlingAnimation.storyName = "Castling (Animated)";
+    return <AnimatedPieces fens={ fens } />;
+  }
+});
 
-export function AppearingAndDisappearingAnimation() {
-  const fens = [
-    EMPTY_POSITION,
-    "n7/n7/n7/n7/n7/n7/n7/n7 w - - 0 1",
-    "nN6/nN6/nN6/nN6/nN6/nN6/nN6/nN6 w - - 0 1",
-    "nNb5/nNb5/nNb5/nNb5/nNb5/nNb5/nNb5/nNb5 w - - 0 1",
-    "nNbB4/nNbB4/nNbB4/nNbB4/nNbB4/nNbB4/nNbB4/nNbB4 w - - 0 1",
-    "nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3/nNbBr3 w - - 0 1",
-    "nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2/nNbBrR2 w - - 0 1",
-    "nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1/nNbBrRq1 w - - 0 1",
-    "nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ/nNbBrRqQ w - - 0 1",
-    "1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ/1NbBrRqQ w - - 0 1",
-    "2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ/2bBrRqQ w - - 0 1",
-    "3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ/3BrRqQ w - - 0 1",
-    "4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ/4rRqQ w - - 0 1",
-    "5RqQ/5RqQ/5RqQ/5RqQ/5RqQ/5RqQ/5RqQ/5RqQ w - - 0 1",
-    "6qQ/6qQ/6qQ/6qQ/6qQ/6qQ/6qQ/6qQ w - - 0 1",
-    "7Q/7Q/7Q/7Q/7Q/7Q/7Q/7Q w - - 0 1"
-  ];
+/**
+ * Capturing animations.
+ */
+export const CapturingAnimation: StoryObj<typeof Pieces> = merge({}, Empty, {
+  name: "Capturing (Animated)",
+  render: () => {
+    const fens = [
+      "KB6/Q5p1/5N2/6R1/1r6/2n5/1P5q/6bk w - - 0 1",
+      "KB6/Q5p1/5N2/6R1/1r6/2P5/7q/6bk b - - 0 1",
+      "KB6/Q7/5p2/6R1/1r6/2P5/7q/6bk w - - 0 2",
+      "KB6/Q7/5p2/6R1/1P6/8/7q/6bk b - - 0 2",
+      "KB6/Q7/8/6p1/1P6/8/7q/6bk w - - 0 3",
+      "K7/Q7/8/6p1/1P6/8/7B/6bk b - - 0 3",
+      "K7/b7/8/6p1/1P6/8/7B/7k w - - 0 4",
+      "8/K7/8/6p1/1P6/8/7B/7k b - - 0 4",
+      "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5",
+      "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5",
+      "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5",
+      "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5"
+    ];
 
-  return <AnimatedPieces fens={ fens } />;
-}
+    return <AnimatedPieces fens={ fens } />;
+  }
+});
 
-AppearingAndDisappearingAnimation.storyName = "Appearing and Disappearing (Animated)";
-
-export function CapturingAnimation() {
-  const fens = [
-    "KB6/Q5p1/5N2/6R1/1r6/2n5/1P5q/6bk w - - 0 1",
-    "KB6/Q5p1/5N2/6R1/1r6/2P5/7q/6bk b - - 0 1",
-    "KB6/Q7/5p2/6R1/1r6/2P5/7q/6bk w - - 0 2",
-    "KB6/Q7/5p2/6R1/1P6/8/7q/6bk b - - 0 2",
-    "KB6/Q7/8/6p1/1P6/8/7q/6bk w - - 0 3",
-    "K7/Q7/8/6p1/1P6/8/7B/6bk b - - 0 3",
-    "K7/b7/8/6p1/1P6/8/7B/7k w - - 0 4",
-    "8/K7/8/6p1/1P6/8/7B/7k b - - 0 4",
-    "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5",
-    "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5",
-    "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5",
-    "8/K7/8/6p1/1P6/8/7k/8 w - - 0 5"
-  ];
-
-  return <AnimatedPieces fens={ fens } />;
-}
-
-CapturingAnimation.storyName = "Capturing (Animated)";
-
-export function RandomAnimation() {
+function RandomAnimationInternal() {
   const [ fen, setFEN ] = useState(EMPTY_POSITION);
 
   useEffect(() => {
@@ -155,4 +180,12 @@ export function RandomAnimation() {
   />;
 }
 
-CapturingAnimation.storyName = "Random (Animated)";
+/**
+ * The pieces randomly appearing, disappearing and moving.
+ */
+export const RandomAnimation: StoryObj<typeof Pieces> = merge({}, Empty, {
+  name: "Random (Animated)",
+  render: () => {
+    return <RandomAnimationInternal />;
+  }
+});
