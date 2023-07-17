8952126ef1d25faaf648ba2168f02ca73d692e95
Landon Schropp <schroppl@gmail.com>
2023-07-16 19:03:01 -0700

    Update Chessboard stories to new format

--- stories/chessboard.stories.tsx
+++ stories/chessboard.stories.tsx
@@ -1,30 +1,39 @@
 import { Chess } from "chess.js";
+import { merge } from "lodash";
 import { useReducer } from "react";
 
 import { Chessboard } from "../src/components/chessboard";
 import { EMPTY_POSITION, STARTING_POSITION, WHITE } from "../src/constants";
 import { useArrowReducer } from "../src/hooks/use-arrow-reducer";
 import { useHighlightReducer } from "../src/hooks/use-highlight-reducer";
+import { useSelectHighlight } from "../src/hooks/use-select-highlight";
 import { MoveEvent } from "../src/types";
+import { SVGBoardDecorator } from "./decorators/svg-decorators";
 
 export default {
   title: "Chessboard",
-  component: Chessboard
+  component: Chessboard,
+  decorators: [ SVGBoardDecorator ]
 };
 
-export function EmptyChessboard() {
-  return <Chessboard
-    fen={ EMPTY_POSITION }
-    orientation={ WHITE }
-  />;
-}
+/**
+ * An empty chessboard.
+ */
+export const Empty = {
+  args: {
+    fen: EMPTY_POSITION,
+    orientation: WHITE
+  }
+};
 
-export function StaticChessboard() {
-  return <Chessboard
-    fen={ STARTING_POSITION }
-    orientation={ WHITE }
-  />;
-}
+/**
+ * A non-interactive chessboard.
+ */
+export const Static = merge({}, Empty, {
+  args: {
+    fen: STARTING_POSITION
+  }
+});
 
 /**
  * A reducer that takes a FEN string and a move event and returns a new FEN string with the move
@@ -39,10 +48,11 @@ function moveReducer(fen: string, move: MoveEvent): string {
   return chess.fen();
 }
 
-export function InteractiveChessboard() {
+function InteractiveChessboard() {
   const [ highlights, handleHighlight ] = useHighlightReducer();
   const [ arrows, handleArrow ] = useArrowReducer();
   const [ fen, dispatchMove ] = useReducer(moveReducer, STARTING_POSITION);
+  const [ selectHighlights, handleSelect, handleDeselect ] = useSelectHighlight();
 
   function handleMove(move: MoveEvent) {
     dispatchMove(move);
@@ -52,11 +62,20 @@ export function InteractiveChessboard() {
   return <Chessboard
     fen={ fen }
     interactive
-    highlights={ highlights }
+    highlights={ [ ...selectHighlights, ...highlights ] }
     arrows={ arrows }
     onHighlight={ handleHighlight }
     onArrow={ handleArrow }
     onMove={ handleMove }
+    onSelect={ handleSelect }
+    onDeselect={ handleDeselect }
     orientation={ WHITE }
   />;
 }
+
+/**
+ * Moveable pieces, highlights, and arrows.
+ */
+export const Interactive = {
+  render: () => <InteractiveChessboard />
+};
