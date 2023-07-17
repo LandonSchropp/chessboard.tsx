b268ae833d48ce6b332c5653514c54e59c93f13a
Landon Schropp <schroppl@gmail.com>
2023-07-16 23:41:54 -0700

    Add tests for useSelect

--- src/hooks/use-select.ts
+++ src/hooks/use-select.ts
@@ -3,12 +3,19 @@ import { useState } from "react";
 
 import { DeselectHandler, Highlight, SelectHandler, SquareEvent } from "../types";
 
+type UseSelectReturnType = [
+  selectedSquare: Square | null,
+  selectedSquareHighlights: Highlight[],
+  handleSelect: SelectHandler,
+  handleDeselect: DeselectHandler,
+]
+
 /**
  * A hook that returns handles and highlights for the currently selected square.
  * @returns Returns an array containing the currently selected square, highlights for the currently
  * selected square, a handler for `onSelect` and a handler for `onDeselect`.
  */
-export function useSelect(): [ Square | null, Highlight[], SelectHandler, DeselectHandler ] {
+export function useSelect(): UseSelectReturnType {
   const [ square, setSquare ] = useState<Square | null>(null);
 
   function handleSelect(event: SquareEvent) {
---
+++ tests/hooks/use-select.test.ts
@@ -0,0 +1,73 @@
+import { renderHook, RenderResult } from "@testing-library/react-hooks";
+
+import { useSelect } from "../../src/hooks/use-select";
+
+describe("useSelect", () => {
+  let result: RenderResult<ReturnType<typeof useSelect>>;
+
+  beforeEach(() => result = renderHook(() => useSelect()).result);
+
+  describe("when the hook is initially called", () => {
+
+    it("returns null for the selected square", () => {
+      expect(result.current[0]).toBeNull();
+    });
+
+    it("returns an empty array for the highlights", () => {
+      expect(result.current[1]).toEqual([]);
+    });
+
+    it("returns a selection handler", () => {
+      expect(result.current[2]).toBeFunction();
+    });
+
+    it("returns a deselection handler", () => {
+      expect(result.current[3]).toBeFunction();
+    });
+  });
+
+  describe("when a square is selected", () => {
+    beforeEach(() => {
+      result.current[2]({ square: "a1" });
+    });
+
+    it("returns the selected square", () => {
+      expect(result.current[0]).toEqual("a1");
+    });
+
+    it("returns a highlight for the selected square", () => {
+      expect(result.current[1]).toEqual([ { square: "a1", shape: "square", type: "select" } ]);
+    });
+
+    it("returns a selection handler", () => {
+      expect(result.current[2]).toBeFunction();
+    });
+
+    it("returns a deselection handler", () => {
+      expect(result.current[3]).toBeFunction();
+    });
+  });
+
+  describe("when a square is deselected", () => {
+    beforeEach(() => {
+      result.current[2]({ square: "a1" });
+      result.current[3]();
+    });
+
+    it("returns null for the selected square", () => {
+      expect(result.current[0]).toBeNull();
+    });
+
+    it("returns an empty array for the highlights", () => {
+      expect(result.current[1]).toEqual([]);
+    });
+
+    it("returns a selection handler", () => {
+      expect(result.current[2]).toBeFunction();
+    });
+
+    it("returns a deselection handler", () => {
+      expect(result.current[3]).toBeFunction();
+    });
+  });
+});
