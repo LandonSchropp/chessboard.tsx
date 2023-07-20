c86aca1f2bfe1d74d408727abdc399c79c1c8cdf
Landon Schropp <schroppl@gmail.com>
2023-07-19 21:59:49 -0700

    Add includes and difference array functions

--- src/utilities/array.ts
+++ src/utilities/array.ts
@@ -6,7 +6,7 @@
  * @returns Returns the array of results from calling the transform function.
  */
 export function times<T>(count: number, transform: (index: number) => T) {
-  return Array(count).fill(null).map((value, index) => transform(index));
+  return Array(count).fill(null).map((_value, index) => transform(index));
 }
 
 /**
@@ -29,4 +29,43 @@ export function minBy<T>(array: T[], transform: (value: T) => number): T | undef
   }
 
   return accumulator;
+}
+
+/**
+ * A helper function that determines whether an array includes a value using the provided isEqual
+ * function.
+ */
+export function includes<T>(
+  array: T[],
+  value: T,
+  isEqual: (first: T, second: T) => boolean
+): boolean {
+  for (const element of array) {
+    if (isEqual(element, value)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/**
+ * Given two arrays and an equality function, this function returns the first array without the
+ * elements in the second array. This uses the provided `isEqual` function to determine if two
+ * elements are the same.
+ */
+export function difference<T>(
+  array1: T[],
+  array2: T[],
+  isEqual: (first: T, second: T) => boolean
+): T[] {
+  const result: T[] = [];
+
+  for (const value of array1) {
+    if (!includes(array2, value, isEqual)) {
+      result.push(value);
+    }
+  }
+
+  return result;
 }
