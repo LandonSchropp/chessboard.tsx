46b9bc5664370b44acf8288b9411f32ff0d5c4c6
Landon Schropp <schroppl@gmail.com>
2023-07-22 22:56:09 -0700

    Add random game Chessboard story

--- stories/chessboard.stories.tsx
+++ stories/chessboard.stories.tsx
@@ -1,14 +1,17 @@
 import { Chess } from "chess.js";
-import { merge } from "lodash";
-import { useReducer } from "react";
+import { merge, sample } from "lodash";
+import { useCallback, useEffect, useReducer } from "react";
 
 import { Chessboard } from "../src/components/chessboard";
 import { EMPTY_POSITION, STARTING_POSITION, WHITE } from "../src/constants";
 import { useArrowReducer } from "../src/hooks/use-arrow-reducer";
 import { useHighlightReducer } from "../src/hooks/use-highlight-reducer";
 import { useLastMove } from "../src/hooks/use-last-move";
+import { useLegalMoves } from "../src/hooks/use-legal-moves";
 import { useSelect } from "../src/hooks/use-select";
-import { MoveEvent } from "../src/types";
+import { BLACK, Move, MoveEvent } from "../src/types";
+import { convertChessJsMoveToMove } from "../src/utilities/chess-js";
+import { playerTurn } from "../src/utilities/fen";
 import { SVGBoardDecorator } from "./decorators/svg-decorators";
 
 export default {
@@ -40,7 +43,7 @@ export const Static = merge({}, Empty, {
  * A reducer that takes a FEN string and a move event and returns a new FEN string with the move
  * applied. This reducer does not check for move legality.
  */
-function reduceMove(fen: string, move: MoveEvent): string {
+function reduceAnyMove(fen: string, move: MoveEvent): string {
   const chess = new Chess(fen);
 
   const piece = chess.remove(move.from);
@@ -54,10 +57,10 @@ function InteractiveChessboard() {
   const [ arrows, handleArrow ] = useArrowReducer();
   const [ , lastMoveHighlights, updateLastMove ] = useLastMove();
   const [ , selectHighlights, handleSelect, handleDeselect ] = useSelect();
-  const [ fen, dispatchMove ] = useReducer(reduceMove, STARTING_POSITION);
+  const [ fen, makeMove ] = useReducer(reduceAnyMove, STARTING_POSITION);
 
   function handleMove(move: MoveEvent) {
-    dispatchMove(move);
+    makeMove(move);
     updateLastMove(move);
     return true;
   }
@@ -77,8 +80,166 @@ function InteractiveChessboard() {
 }
 
 /**
+ * Returns true if the move is legal from the FEN position.
+ */
+function isLegalMove(fen: string, { from, to }: Move): boolean {
+  const chess = new Chess(fen);
+
+  try {
+    chess.move({ from, to });
+  }
+  catch (error) {
+    if (error instanceof Error && error.message.startsWith("Invalid move")) {
+      return false;
+    }
+
+    throw error;
+  }
+
+  return true;
+}
+
+/**
+ * A reducer that takes a FEN string and a move event and returns a new FEN string with the move
+ * applied. This reducer will not allow illegal moves.
+ */
+function reduceLegalMove(fen: string, move: MoveEvent): string {
+  if (!isLegalMove(fen, move)) {
+    return fen;
+  }
+
+  const chess = new Chess(fen);
+  chess.move({ from: move.from, to: move.to });
+  return chess.fen();
+}
+
+/**
  * Moveable pieces, highlights, and arrows.
  */
 export const Interactive = {
   render: () => <InteractiveChessboard />
+};
+
+function LegalMovesChessboard() {
+  const [ highlights, handleHighlight ] = useHighlightReducer();
+  const [ arrows, handleArrow ] = useArrowReducer();
+  const [ , lastMoveHighlights, updateLastMove ] = useLastMove();
+  const [ selectedSquare, selectHighlights, handleSelect, handleDeselect ] = useSelect();
+  const [ fen, makeMove ] = useReducer(reduceLegalMove, STARTING_POSITION);
+  const [ , legalMoveHighlights ] = useLegalMoves(fen, selectedSquare);
+
+  function handleMove(move: MoveEvent) {
+    if (!isLegalMove(fen, move)) {
+      return false;
+    }
+
+    makeMove(move);
+    updateLastMove(move);
+
+    return true;
+  }
+
+  return <Chessboard
+    fen={ fen }
+    interactive
+    highlights={
+      [
+        ...lastMoveHighlights,
+        ...selectHighlights,
+        ...legalMoveHighlights,
+        ...highlights
+      ]
+    }
+    arrows={ arrows }
+    onHighlight={ handleHighlight }
+    onArrow={ handleArrow }
+    onMove={ handleMove }
+    onSelect={ handleSelect }
+    onDeselect={ handleDeselect }
+    orientation={ WHITE }
+  />;
+}
+
+/**
+ * An interactive chessboard that only allows legal chess moves.
+ */
+export const LegalMoves = {
+  render: () => <LegalMovesChessboard />
+};
+
+/**
+ * Returns a random legal move from the FEN position, or null if there are no legal moves available.
+ */
+function randomLegalMove(fen: string): Move | null {
+  const moves = new Chess(fen).moves({ verbose: true });
+  const move = sample(moves);
+
+  if (!move) {
+    return null;
+  }
+
+  return convertChessJsMoveToMove(move);
+}
+
+function RandomGameChessboard() {
+  const [ highlights, handleHighlight ] = useHighlightReducer();
+  const [ arrows, handleArrow ] = useArrowReducer();
+  const [ , lastMoveHighlights, updateLastMove ] = useLastMove();
+  const [ selectedSquare, selectHighlights, handleSelect, handleDeselect ] = useSelect();
+  const [ fen, makeMove ] = useReducer(reduceLegalMove, STARTING_POSITION);
+  const [ , legalMoveHighlights ] = useLegalMoves(fen, selectedSquare);
+
+  const handleMove = useCallback((move: MoveEvent) => {
+    if (!isLegalMove(fen, move)) {
+      return false;
+    }
+
+    makeMove(move);
+    updateLastMove(move);
+
+    return true;
+  }, [ fen, makeMove, updateLastMove ]);
+
+  useEffect(() => {
+    if (playerTurn(fen) === WHITE) {
+      return;
+    }
+
+    setTimeout(() => {
+      const move = randomLegalMove(fen);
+
+      if (!move) {
+        return;
+      }
+
+      handleMove(move);
+    }, 1000);
+  }, [ fen, handleMove ]);
+
+  return <Chessboard
+    fen={ fen }
+    interactive
+    highlights={
+      [
+        ...lastMoveHighlights,
+        ...selectHighlights,
+        ...legalMoveHighlights,
+        ...highlights
+      ]
+    }
+    arrows={ arrows }
+    onHighlight={ handleHighlight }
+    onArrow={ handleArrow }
+    onMove={ handleMove }
+    onSelect={ handleSelect }
+    onDeselect={ handleDeselect }
+    orientation={ WHITE }
+  />;
+}
+
+/**
+ * A chess game where the computer opponent plays random moves.
+ */
+export const RandomGame = {
+  render: () => <RandomGameChessboard />
 };
